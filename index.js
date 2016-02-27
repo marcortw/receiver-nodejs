80c4e97439a37fb1d4c4ee89024056c3a56599df
marcortw <github@schnueriger.net>
2016-02-27 15:04:24 +0100

    Support for the ACDPWRAPPER / encryption message

--- index.js
+++ index.js
@@ -2,9 +2,44 @@ var logger = require('./lib/logger.js');
 var multicastListener = require('./lib/listeners/multicast.js');
 var unicastListener = require('./lib/listeners/unicast.js');
 var collector = require('./lib/requestCollector');
+var config = require('./lib/configloader');
 
 const EventEmitter = require("events");
 const util = require('util');
+
+
+// user supplied config, e.g. jwe key
+var initialized = false;
+
+/**
+ *
+ * @param options
+ * @param callback
+ * @returns {*}
+ */
+var init = function (options, callback) {
+    var retMsg = "";
+    if (!initialized) {
+        if (options.key) {
+            try {
+                var key = JSON.stringify(options.key);
+                config.set('encryption:jwekey', key);
+                retMsg += 'Supplied key has been added to the configuration. '
+            } catch (e) {
+                var errMsg = 'Supplied key is not a valid JSON structure.';
+                logger.error(e);
+                return new Error(errMsg)
+            }
+        }
+        logger.trace("CONFIGURATION AFTER USER-INIT: " + JSON.stringify(config.get()));
+        return retMsg;
+
+    } else {
+        var msg = 'User-supplied config has already been initialized!!';
+        logger.error(msg);
+        return new Error(msg);
+    }
+};
 
 multicastListener.start();
 unicastListener.start();
@@ -47,6 +82,7 @@ function externalResponseHandler(externalResponseFunction) {
 }
 
 module.exports = {
+    init: init,
     controller: myEmitter,
     respond: externalResponseHandler
 };
