1df76dfe9d994bc514c0e0bd8d1be5cbe1d42ea1
marcortw <github@schnueriger.net>
2016-03-01 22:17:39 +0100

    Bugfix regarding error handling if message was not possible to unwrap/decrypt

--- index.js
+++ index.js
@@ -18,26 +18,44 @@ var initialized = false;
  * @returns {*}
  */
 var init = function (options, callback) {
-    var retMsg = "";
-    if (!initialized) {
-        if (options.key) {
-            try {
-                var key = JSON.stringify(options.key);
-                config.set('encryption:jwekey', key);
-                retMsg += 'Supplied key has been added to the configuration. '
-            } catch (e) {
-                var errMsg = 'Supplied key is not a valid JSON structure.';
-                logger.error(e);
-                return new Error(errMsg)
-            }
-        }
-        logger.trace("CONFIGURATION AFTER USER-INIT: " + JSON.stringify(config.get()));
-        return retMsg;
+    var errors = [];
 
+    // do some sync stuff, fill errors[] if errors happen (may partially work)
+    if (!initialized) {
+        if (options) {
+            if (options.key) {
+                try {
+                    var key = JSON.stringify(options.key);
+                    config.set('encryption:jwekey', key);
+                    logger.debug('Supplied key has been added to the configuration.');
+                } catch (e) {
+                    var keyParsingError = new Error('Supplied key is not a valid JSON structure.');
+                    logger.error(keyParsingError);
+                    errors.push(keyParsingError);
+                }
+            }
+            //TODO other options
+        } else {
+            var noOptionsSuppliedError = new Error('No initialization options were supplied.');
+            logger.error(noOptionsSuppliedError);
+            errors.push(noOptionsSuppliedError);
+        }
     } else {
-        var msg = 'User-supplied config has already been initialized!!';
-        logger.error(msg);
-        return new Error(msg);
+        var noOptionsSuppliedError = new Error('User-supplied config has already been initialized!');
+        logger.error(noOptionsSuppliedError);
+        errors.push(noOptionsSuppliedError);
+    }
+
+    initialized = true;
+    if (errors.length > 0) {
+        logger.trace("CONFIGURATION AFTER USER-INIT: " + JSON.stringify(config.get()));
+        if (callback) callback(errors);
+        return errors;
+    } else {
+        logger.trace("CONFIGURATION AFTER USER-INIT: " + JSON.stringify(config.get()));
+        var okmsg = "everything went fine";
+        if (callback) callback(null, okmsg);
+        return okmsg;
     }
 };
 
@@ -73,6 +91,10 @@ function notificationHandler(options, acdprequest, callback) {
     userCallback
 }
 
+/**
+ *
+ * @param externalResponseFunction must be a function which takes three parameters (options, acdprequest, callback). Delegates control over the requests to the implementer
+ */
 function externalResponseHandler(externalResponseFunction) {
     if (externalResponseFunction.length != 3) {
         logger.error('Response handler function must be constructed with {options},{acdprequest},callback().')
